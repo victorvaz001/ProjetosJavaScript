c6eca7d64072169cbc6126f12cb83597a5a2d1db
Victor Gonçalves <victorvaz001@gmail.com>
2020-08-18 16:58:44 -0300

    Alteração do script.js, adicionando lista de produtos

---
+++ Manipulando Listas/script.js
@@ -0,0 +1,13 @@
+function adicionarItem(){
+	var prod =  document.getElementById("produto").value;
+
+	var listahtml = document.getElementById("lista").innerHTML;
+
+	listahtml = listahtml + "<li>"+prod+"</li>";
+
+	document.getElementById("lista").innerHTML = listahtml;
+
+
+
+}
+
